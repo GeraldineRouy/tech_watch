8fab0705983a1fc10ecff833d55512a3c59d3b3b
geraldine.rouy <geraldine.rouy@gmail.com>
2025-02-26 13:14:09 +0100

    first commit

---
+++ src/main/java/org/jaalon/tech_watch/TechWatchApplication.java
@@ -0,0 +1,13 @@
+package org.jaalon.tech_watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechWatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechWatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jaalon/tech_watch/TechWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jaalon.tech_watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechWatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
